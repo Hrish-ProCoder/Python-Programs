d9936ce21bb789b1832df86d7fe4891920691fc2
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-04 07:03:09 +0530

    using numpy

--- Determinant of 2X2 matrix.py
+++ Determinant of 2X2 matrix.py
@@ -1,10 +1,14 @@
+# import pakages
 import numpy as np
 from numpy import linalg
 
+# array
 mat = np.array([[2, 6], [1, 4]])
 
 print("Original 2D Matrix")
 print(mat)
 
+
 print("Determinant of the 2-D Matrix:")
+# linal.det for output
 print(np.linalg.det(mat))
