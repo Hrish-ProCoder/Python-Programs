7f516e5106d4ad9fd04c841c9c7966db39bd637a
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-11 08:16:49 +0530

    Update text image on Button.py

--- text image on Button.py
+++ text image on Button.py
@@ -1,7 +1,11 @@
 from tkinter import * 
 from tkinter.ttk import *
 root = Tk()
+
 Label(root, text = 'Button', font =('Montera', 16)).pack(side = TOP, pady = 10)
 photo = PhotoImage(file = r"C:\Desktop\car.jpg")
+
 photoimage = photo.subsample(3, 3)
 Button(root, text = 'Click Here', image = photoimage, compound = LEFT).pack(side = TOP)
+
+mainloop()
